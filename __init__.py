62fe5831ddd5e3e4c6a885aa21bf41355abb12ce
Yaroslav Shirokov <slava@slavasaur.com>
2010-03-02 15:14:53 -0500

    Can now apply roles to tasks

--- __init__.py
+++ __init__.py
@@ -1,12 +1,23 @@
 from fabric.api import *
 from fabric.network import needs_host
 
+def with_roles(*roles_then_funcs):
+    def role_or_function(acc, i):
+        if callable(i): acc[1].append(i)
+        else: acc[0].append(i)
+        return acc
+    def bind_roles(roles_list, func):
+        func.roles = roles_list
+    required_roles, functions = reduce(role_or_function, roles_then_funcs, ([], []))
+    map(lambda func: bind_roles(required_roles, func), functions)
+
 @needs_host
 def setup():
     '''
     Create all the required directories used by a deployment
     '''
     require("path")
+    send_release()
     pass
 
 
